94b74ce726bcf75ff783ec590c4ab6a2599d87b3
Adarsh Singh <adarsh04455@gmail.com>
2026-07-31 22:52:36 +0530

    perf(core): Day 01 programms

--- Basic/Day-01/HollowButterfly.py
+++ Basic/Day-01/HollowButterfly.py
@@ -1,35 +1,43 @@
 n = int(input("enter a number: "))
 
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        if j==1 or i == j:
-            print(j, end="")
-        else:
-            print(" ",end="")
-    for k in range(2*(n-i)):
-        print(" ", end="")
-
-    for l in range(i+1):
-        if l==1 or l==i:
-            print(l, end="")
-        else:
-            print(" ",end="")
-    print()
-
- # for below portion of butterfly
-for i in range(n,0,-1):
-    for j in range(1,i+1):
-        if j==1 or i==j:
-            print(j,end="")
-        else:
-            print(" ",end="")
-    for k in range(2*(n-i),0,-1):
-        print("_", end="")
-
-    for j in range(n,0,-1):
-        if j ==1 or j==i:
-            print(j, end="")
+# upper half
+for i in range(1, n+1):
+    # left wing
+    for j in range(1, i+1):
+        if j == 1 or j == i:
+            print("*", end="")
         else:
             print(" ", end="")
 
+    # middle spaces
+    for _ in range(2 * (n - i)):
+        print(" ", end="")
+
+    # right wing
+    for j in range(1, i+1):
+        if j == 1 or j == i:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
+
+# lower half
+for i in range(n, 0, -1):
+    # left wing
+    for j in range(1, i+1):
+        if j == 1 or j == i:
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+    # middle spaces
+    for _ in range(2 * (n - i)):
+        print(" ", end="")
+
+    # right wing
+    for j in range(1, i+1):
+        if j == 1 or j == i:
+            print("*", end="")
+        else:
+            print(" ", end="")
     print()
