34241078c1da70620587bf4290bf399434d3032b
Adarsh Singh <adarsh04455@gmail.com>
2026-07-31 22:50:16 +0530

    perf(core): Day 02 programms

--- Basic/Day-01/HollowButterfly.py
+++ Basic/Day-01/HollowButterfly.py
@@ -3,7 +3,7 @@ n = int(input("enter a number: "))
 for i in range(1,n+1):
     for j in range(1,i+1):
         if j==1 or i == j:
-            print("*", end="")
+            print(j, end="")
         else:
             print(" ",end="")
     for k in range(2*(n-i)):
@@ -11,7 +11,7 @@ for i in range(1,n+1):
 
     for l in range(i+1):
         if l==1 or l==i:
-            print("*", end="")
+            print(l, end="")
         else:
             print(" ",end="")
     print()
@@ -20,8 +20,16 @@ for i in range(1,n+1):
 for i in range(n,0,-1):
     for j in range(1,i+1):
         if j==1 or i==j:
-            print("*",end="")
+            print(j,end="")
         else:
             print(" ",end="")
+    for k in range(2*(n-i),0,-1):
+        print("_", end="")
+
+    for j in range(n,0,-1):
+        if j ==1 or j==i:
+            print(j, end="")
+        else:
+            print(" ", end="")
 
     print()
---
+++ Basic/Day-02/NumberPyramid.py
@@ -0,0 +1,13 @@
+n=5
+
+for i in range(1,n+1):
+    for k in range(n,i,-1):
+        print(" ",end="")
+
+    for j in range(1,i+1):
+        print(j , end="")
+
+    for k in range(i-1,0,-1):
+        print(k,end="")
+
+    print()
