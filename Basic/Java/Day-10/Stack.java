2782a2ec87a28c4d61c75bb73bb617ef680867cf
Adarsh Singh <adarsh04455@gmail.com>
2026-08-09 21:45:06 +0530

    perf(core): Day 10 programms

---
+++ Basic/Java/Day-10/Stack.java
@@ -0,0 +1,19 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Stack{
+    public static void main(String[] args) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(12);
+        stack.push(13);
+        stack.push(14);
+        stack.push(16);
+        System.out.println(stack);
+        System.out.println("See top element before deletion : " + stack.peek());
+        System.out.println("Poped top element : " + stack.pop());
+        System.out.println("See top element after deletion : " + stack.peek());
+        System.out.println("Check stack is empty or not : " + stack.isEmpty());
+        System.out.println("Check size of stack : " + stack.size());
+        System.out.println(stack);
+    }
+}
