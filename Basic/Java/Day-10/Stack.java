9f6fdfa22a09b1c63a2ff5c7636dc749047acf05
Adarsh Singh <adarsh04455@gmail.com>
2026-08-09 22:19:27 +0530

    perf(core): Day 10 programms

--- Basic/Java/Day-10/Stack.java
+++ Basic/Java/Day-10/Stack.java
@@ -10,7 +10,7 @@ public class Stack{
         stack.push(16);
         System.out.println(stack);
         System.out.println("See top element before deletion : " + stack.peek());
-        System.out.println("Poped top element : " + stack.pop());
+        System.out.println("Popped top element : " + stack.pop());
         System.out.println("See top element after deletion : " + stack.peek());
         System.out.println("Check stack is empty or not : " + stack.isEmpty());
         System.out.println("Check size of stack : " + stack.size());
