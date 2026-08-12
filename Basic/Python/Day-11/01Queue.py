764b882a7f267a7dac821e34c6fd4539e29752e2
Adarsh Singh <adarsh04455@gmail.com>
2026-08-12 14:10:48 +0530

    perf(core): Day 11 programms

--- Basic/Python/Day-11/01Queue.py
+++ Basic/Python/Day-11/01Queue.py
@@ -8,12 +8,12 @@ queue.append(30)
 
 print(queue)
 
-print(queue[0])
+print(queue[0]) # used to view the top element
 
-print(queue.popleft())
+print(queue.popleft()) # used to pop the element from starting with O(1) TC.
 
 print(queue)
 
-print(len(queue))
+print(len(queue)) # return length of the queue...
 
-print(not queue)
+print(not queue) # return false if queue is not empty
