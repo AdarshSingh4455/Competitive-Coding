e59e9e57504f4c1c2e80db8aea492dfc50c85452
Adarsh Singh <adarsh04455@gmail.com>
2026-08-12 14:08:09 +0530

    perf(core): Day 11 programms

--- Basic/Python/Day-11/01Queue.py
+++ Basic/Python/Day-11/01Queue.py
@@ -3,8 +3,17 @@ from collections import deque
 queue = deque()
 
 queue.append(10)
-queue.append(15)
-queue.append(21)
+queue.append(20)
+queue.append(30)
+
+print(queue)
+
+print(queue[0])
 
 print(queue.popleft())
+
 print(queue)
+
+print(len(queue))
+
+print(not queue)
