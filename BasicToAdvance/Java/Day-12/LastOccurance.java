dcc9f469b52e15809ffe3728e29bfffffb5ecf48
Adarsh Singh <adarsh04455@gmail.com>
2026-08-16 10:54:17 +0530

    perf(core): Day 12 programms

---
+++ BasicToAdvance/Java/Day-12/LastOccurance.java
@@ -0,0 +1,29 @@
+public class LastOccurance {
+    public static void main(String[] args) {
+        int[] arr = {12,18,26,45,45,62};
+        int target = 45;
+
+        int x = binarySearch(arr,target);
+        System.out.println(x);
+    }
+
+    public static int binarySearch(int[] arr, int target){
+        int answer = -1;
+        int n = arr.length;
+        int high = n-1;
+        int low = 0;
+
+        while (low<=high) {
+            int mid = (low + high)/2;
+            if (arr[mid]==target) {
+                answer = mid;
+                low = mid+1;
+            }else if (arr[mid]<target) {
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return answer;
+    }
+}
