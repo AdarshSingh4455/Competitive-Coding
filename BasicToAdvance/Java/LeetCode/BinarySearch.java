fe962d65af3af93d922c439206f595b219f94277
Adarsh Singh <adarsh04455@gmail.com>
2026-08-24 15:36:10 +0530

    perf(core): Leetcode programms

---
+++ BasicToAdvance/Java/LeetCode/BinarySearch.java
@@ -0,0 +1,34 @@
+public class BinarySearch {
+	private int[] nums;
+	private int target;
+
+	public BinarySearch(int[] nums, int target) {
+		this.nums = nums;
+		this.target = target;
+	}
+
+	public int search(int[] nums, int target) {
+		int low = 0;
+		int high = nums.length - 1;
+
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			} else if (nums[mid] > target) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = {-1, 0, 3, 5, 9, 12};
+		int target = 9;
+		BinarySearch solution = new BinarySearch(nums, target);
+		System.out.println(solution.search(solution.nums, solution.target));
+	}
+}
