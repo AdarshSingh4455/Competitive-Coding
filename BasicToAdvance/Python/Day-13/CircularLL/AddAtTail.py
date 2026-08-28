a24c5a9ff09a7437a219908a81cdd6b6352cff6f
Adarsh Singh <adarsh04455@gmail.com>
2026-08-28 19:18:54 +0530

    perf(core): Day 13 programms

---
+++ BasicToAdvance/Python/Day-13/CircularLL/AddAtTail.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        self.tail = None
+
+class CircularLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def addAtTail(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next = new_node
+            return
+
+        else:
+            self.tail.next = new_node
+            new_node.next = self.head
+            self.tail = new_node
+
+cll = CircularLinkedList()
+cll.addAtTail(5)
+cll.addAtTail(10)
+cll.addAtTail(20)
+cll.addAtTail(30)
+
+current = cll.head
+while True:
+    print(current.data)
+    current = current.next
+    if current == cll.head:
+        break
