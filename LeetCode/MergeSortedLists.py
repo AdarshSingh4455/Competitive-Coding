71f596849e823c7b7d7bf7efc8f702dc85105196
Adarsh Singh <adarsh04455@gmail.com>
2026-09-01 11:58:11 +0530

    perf(core): Leetcode programms

--- LeetCode/MergeSortedLists.py
+++ LeetCode/MergeSortedLists.py
@@ -5,11 +5,6 @@ class ListNode(object):
         self.next = next
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
         dummy = ListNode(0)
         current = dummy
 
